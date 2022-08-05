106117e3b5e73941542a85830fb6e228bcc5f5a7
Kelvin Steiner <me@steinerkelvin.dev>
2022-08-05 17:18:03 -0300

    dunno

--- apps/explorer/next.config.js
+++ apps/explorer/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+// const withTM = require('next-transpile-modules')(['kindelia'], {
+//   debug: true,
+// });
+
+// const nextConfig = withTM({
+//   reactStrictMode: true,
+//   swcMinify: true,
+// })
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
