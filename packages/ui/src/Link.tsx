60a221e3359377ecb92fd04c048501610691d82f
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-08-23 00:46:01 -0300

    chore: add cy

--- packages/ui/src/Link.tsx
+++ packages/ui/src/Link.tsx
@@ -2,13 +2,15 @@ type LinkProps = {
   href: string;
   children: React.ReactNode;
   className?: string;
+  cy?: string;
 };
 
 export function Link(props: LinkProps): JSX.Element {
-  const {href, children, className} = props;
+  const {href, children, className, cy} = props;
 
   return (
     <a
+      data-cy={cy}
       href={href}
       className={`text-springwood text-center text-base font-semibold capitalize ${className}`}>
       {children}
