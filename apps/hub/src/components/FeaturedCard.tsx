b8e136b8ca6e5f451af77c9031f146a9ee489d15
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-08-28 03:46:49 -0300

    chore: create FeaturedCard

---
+++ apps/hub/src/components/FeaturedCard.tsx
@@ -0,0 +1,23 @@
+import {FiHeart, Text} from 'ui';
+
+export type FeaturedCardProps = {
+  key?: string | number;
+};
+
+export function FeaturedCard(props: FeaturedCardProps): JSX.Element {
+  const {key} = props;
+
+  return (
+    <div key={key} className="h-[441px] w-[326px] rounded-md bg-white p-8">
+      <div className="flex items-center justify-between">
+        <Text className="text-mineshaft text-sm">Rodrigo O.</Text>
+        <div className="flex items-center gap-2">
+          <FiHeart color="rgb(209, 213,219)" fontSize={14} />
+          <Text className="text-xs text-gray-300">250</Text>
+        </div>
+      </div>
+      <Text className="text-xs text-gray-300">2D Pixel Art Game</Text>
+      <div className="bg-mineshaft h-64 w-64 rounded-lg mt-4"></div>
+    </div>
+  );
+}
