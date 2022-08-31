a1cbe39ee9db13b01a3eae5d74758c35e0f5ce94
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-08-31 14:44:18 -0300

    chore: remove SVG and add LogoIcon

--- apps/home/src/pages/index.tsx
+++ apps/home/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import {SVG, Footer} from 'ui';
+import {Footer, LogoIcon} from 'ui';
 import {Menu} from '../components/Menu';
 
 export default function Home(): JSX.Element {
@@ -11,7 +11,7 @@ export default function Home(): JSX.Element {
 
       <main className="mx-auto flex w-auto flex-col items-center px-4 pt-16 sm:pt-24 lg:px-8">
         <div className="mb-36 flex cursor-pointer">
-          <SVG type="logo" />
+          <LogoIcon className='w-4/5 h-4/5 active:animate-bounce' />
         </div>
         <Menu />
       </main>
