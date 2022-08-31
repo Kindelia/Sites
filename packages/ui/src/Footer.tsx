ca5aa00f51b9f4fa559cab9d5c94bd32cdb8d5da
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-08-31 14:45:34 -0300

    chore: remove react-icons

--- packages/ui/src/Footer.tsx
+++ packages/ui/src/Footer.tsx
@@ -1,41 +1,36 @@
-import {IconContext} from 'react-icons';
-import {FaDiscord, FaTwitter, FaYoutube, FaGithub} from 'react-icons/fa';
 import {Link} from './Link';
+import {DiscordIcon} from './Icon/DiscordIcon';
+import {GithubIcon} from './Icon/GithubIcon';
+import {TwitterIcon} from './Icon/TwitterIcon';
+import {YoutubeIcon} from './Icon/YoutubeIcon';
 
 export function Footer(): JSX.Element {
   const fullYear = new Date().getFullYear();
 
   return (
-    <IconContext.Provider
-      value={{
-        size: '2rem',
-        color: 'rgba(247, 243, 240, 0.5)',
-      }}
-    >
-      <footer className="bg-mineshaft mt-auto flex h-24 w-full items-center justify-around">
-        <div>
-          <text className="text-springwood font-inter text-base font-semibold leading-8 opacity-50">
-            © {fullYear} Kindelia Foundation
-          </text>
-        </div>
-        <div className="flex items-center gap-4">
-          <text className="text-springwood font-inter text-base font-semibold leading-8 opacity-50">
-            Follow Us
-          </text>
-          <Link href="https://twitter.com/KindeliaOrg" cy="twitter">
-            <FaTwitter />
-          </Link>
-          <Link href="https://www.youtube.com/c/Kindelia" cy="youtube">
-            <FaYoutube />
-          </Link>
-          <Link href="https://discord.gg/kindelia" cy="discord">
-            <FaDiscord />
-          </Link>
-          <Link href="https://github.com/Kindelia" cy="github">
-            <FaGithub />
-          </Link>
-        </div>
-      </footer>
-    </IconContext.Provider>
+    <footer className="bg-mineshaft mt-auto flex h-24 w-full items-center justify-around">
+      <div>
+        <text className="text-springwood font-inter text-base font-semibold leading-8 opacity-50">
+          © {fullYear} Kindelia Foundation
+        </text>
+      </div>
+      <div className="flex items-center gap-4">
+        <text className="text-springwood font-inter text-base font-semibold leading-8 opacity-50">
+          Follow Us
+        </text>
+        <Link href="https://github.com/Kindelia" cy="github">
+          <GithubIcon  className='h-8 w-8 hover:text-springwood'/>
+        </Link>
+        <Link href="https://twitter.com/KindeliaOrg" cy="twitter">
+          <TwitterIcon className='h-8 w-8 hover:text-springwood' />
+        </Link>
+        <Link href="https://www.youtube.com/c/Kindelia" cy="youtube">
+          <YoutubeIcon className='h-8 w-8 hover:text-springwood' />
+        </Link>
+        <Link href="https://discord.gg/kindelia" cy="discord">
+          <DiscordIcon className='h-8 w-8 hover:text-springwood' />
+        </Link>
+      </div>
+    </footer>
   );
 }
