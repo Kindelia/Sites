f9618f343846221e175f95654f017ee2151942e6
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-09-02 02:13:36 -0300

    chore: add _document

---
+++ apps/kind2/src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    return Document.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html className="bg-springwood">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
