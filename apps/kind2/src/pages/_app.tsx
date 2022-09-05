e456e652a7fb9acbbe230c02615a3f083adc16c4
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-09-05 14:10:48 -0300

    chore: fix _app.tsx

--- apps/kind2/src/pages/_app.tsx
+++ apps/kind2/src/pages/_app.tsx
@@ -2,12 +2,18 @@ import '../styles/globals.css';
 import 'ui/styles.css';
 
 import type {AppProps} from 'next/app';
-import { PublicLayout } from '../layouts/PublicLayout';
+import {PublicLayout} from '../layouts/PublicLayout';
+import {DocLayout} from '../layouts/DocLayout';
+import {useRouter} from 'next/router';
 
 export default function MyApp({Component, pageProps}: AppProps) {
+  const router = useRouter();
+
+  const Layout = router.pathname.includes('/docs') ? DocLayout : PublicLayout;
+
   return (
-    <PublicLayout title='test'>
+    <Layout title="Kind2 | Kindelia">
       <Component {...pageProps} />
-    </PublicLayout>
-  )
+    </Layout>
+  );
 }
