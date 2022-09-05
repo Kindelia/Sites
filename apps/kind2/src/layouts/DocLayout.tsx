076000652f3af7ee86ca37892dd4fc7011b06997
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-09-05 14:10:21 -0300

    chore: create DocLayout

--- apps/kind2/src/layouts/DocLayout.tsx
+++ apps/kind2/src/layouts/DocLayout.tsx
@@ -1,27 +1,31 @@
-import {Footer} from 'ui';
-import {MenuDocs} from '../components/MenuDocs';
 import clsx from 'clsx';
-import Router from 'next/router';
+import {Footer} from 'ui';
 
+import {MenuDocs} from '../components/MenuDocs';
 import {Navbar} from '../components/Navbar';
 
-export type PublicLayoutProps = {
+export type DocLayoutProps = {
   children: React.ReactNode;
   title: string;
 };
 
-export function PublicLayout(props: PublicLayoutProps): JSX.Element {
+export function DocLayout(props: DocLayoutProps): JSX.Element {
   const {children} = props;
 
-  const isDoc = true
+  const isDoc = true;
 
   const className = isDoc ? 'flex-row items-center justify-center' : '';
 
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      <main className={clsx('flex w-full flex-col bg-pink-500', className)}>
-        {children}
+      <main
+        className={clsx(
+          'flex w-full flex-row',
+          className,
+        )}
+      >
+        <div className="border-mineshaft border-2 flex flex-1 min-h-screen p-32 flex-col">{children}</div>
         {isDoc && <MenuDocs />}
       </main>
       <Footer />
