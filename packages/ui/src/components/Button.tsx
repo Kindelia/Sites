2030ea1ad4c6a863f050b7c29e1c1c963d44d3f1
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-09-15 15:38:30 -0300

    chore: improve secondary button and add twMerge

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -1,23 +1,23 @@
-import clsx from 'clsx';
+import {twMerge} from 'tailwind-merge';
 
 type ButtonProps = React.ComponentPropsWithRef<'button'> & {
   variant?: 'primary' | 'secondary';
-}
+};
 
 export function Button(props: ButtonProps): JSX.Element {
-  const {children, variant, ...restProps} = props;
+  const {children, className, variant, ...restProps} = props;
 
   const style =
     variant === 'secondary'
-      ? 'bg-white text-mineshaft'
+      ? 'bg-transparent text-mineshaft border border-mineshaft'
       : 'bg-black text-springwood';
 
   return (
     <button
-      className={clsx(
-        'font-inter rounded-2xl border-none px-4 py-2 text-base font-semibold',
+      className={twMerge(
+        'font-inter rounded-lg px-4 py-2 text-base font-semibold',
         style,
-        props.className,
+        className,
       )}
       {...restProps}
     >
